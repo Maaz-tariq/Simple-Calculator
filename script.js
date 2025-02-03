bae363462481e4a95bbb71d32c6fe9a8c80dedf8
Maaz-tariq <maazt254@gmail.com>
2025-02-03 09:41:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let displayValue = '';
+
+function appendNumber(number) {
+    displayValue += number;
+    document.getElementById('display').value = displayValue;
+}
+
+function appendOperator(operator) {
+    displayValue += operator;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue).toString();
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+        displayValue = '';
+    }
+}
